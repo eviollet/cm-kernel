7bbcf35ccc1d1e476fae660c1b8da2f01a1b7d2c
Emmanuel Viollet <Emmanuel.Viollet@gmail.com>
2011-04-07 22:14:10 +0200

    Upped BlueTooth audio output Added two-way recording thanks to avs333 (http://forum.xda-developers.com/showthread.php?t=993793) Kernels are now signed

--- mkflash.sh
+++ mkflash.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+#set -x
 
+sdkDirectory=/usr/local/bin/android-sdk-linux_x86
 
 localVersion=`cat .config | fgrep CONFIG_LOCALVERSION= | cut -f 2 -d= | sed s/\"//g`
 linuxVersion=`cat .config | fgrep "Linux kernel version:" | cut -d: -f 2 | cut -c2-`
@@ -37,3 +39,15 @@ rm -f kernel.zip 2> /dev/null
 cd flash
 zip -r --quiet  ../kernel.zip .
 cd ..
+
+fullName="$PWD/kernel.zip"
+signedName="$PWD/kernel-signed.zip"
+#tmpName="/tmp/kernel-signed.zip"
+cd $sdkDirectory/tools/SignApk
+#./androsign $fullName > /dev/null
+java -jar signapk.jar certificate.pem key.pk8 $fullName $signedName
+#rm -f $fullName
+#mv $tmpName $fullName
+#cd -
+#rm -f kernel.zip
+#mv kernel-signed.zip kernel.zip > /dev/null
