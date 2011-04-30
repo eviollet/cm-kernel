c3f51393b1116e91d72c05658d98b9de8835bd26
Emmanuel Viollet <Emmanuel.Viollet@gmail.com>
2011-05-01 00:05:49 +0200

    2.6.35.12 porting...

--- fs/ioprio.c
+++ fs/ioprio.c
@@ -60,17 +60,17 @@ int set_task_ioprio(struct task_struct *task, int ioprio)
 			err = -ENOMEM;
 			break;
 		}
-	/* let other ioc users see the new values */
+		/* let other ioc users see the new values */
 		smp_wmb();
 		task->io_context = ioc;
 	} while (1);
 
 	if (!err) {
 		ioc->ioprio = ioprio;
-	/* make sure schedulers see the new ioprio value */
-	wmb();
-	for (i = 0; i < IOC_IOPRIO_CHANGED_BITS; i++)
-		set_bit(i, ioc->ioprio_changed);
+		/* make sure schedulers see the new ioprio value */
+		wmb();
+		for (i = 0; i < IOC_IOPRIO_CHANGED_BITS; i++)
+			set_bit(i, ioc->ioprio_changed);
 	}
 
 	task_unlock(task);
