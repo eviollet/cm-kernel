8b8c1fcc1cc2390608ecdf9cfd000f6bde41df67
Emmanuel Viollet <Emmanuel.Viollet@gmail.com>
2011-10-03 00:18:18 +0200

    Set minimum AVS voltages to 1000mv and max voltages to 1350

--- arch/arm/mach-msm/board-bravo.c
+++ arch/arm/mach-msm/board-bravo.c
@@ -50,6 +50,7 @@
 #include <mach/msm_flashlight.h>
 #include <mach/vreg.h>
 #include <mach/board-bravo-microp-common.h>
+#include <mach/board_htc.h>
 
 #include "board-bravo.h"
 #include "devices.h"
@@ -1085,7 +1086,7 @@ static int __init parse_tag_bdaddr(const struct tag *tag)
 
 __tagtable(ATAG_BDADDR, parse_tag_bdaddr);
 
-static int __init board_serialno_setup(char *serialno)
+static int __init serialno_setup(char *serialno)
 {
 #ifdef CONFIG_USB_ANDROID_RNDIS
 	int i;
@@ -1104,7 +1105,6 @@ static int __init board_serialno_setup(char *serialno)
 	android_usb_pdata.serial_number = serialno;
 	return 1;
 }
-__setup("androidboot.serialno=", board_serialno_setup);
 
 static void config_gpio_table(uint32_t *table, int len)
 {
@@ -1219,6 +1219,8 @@ static void __init bravo_init(void)
 	platform_device_register(&bravo_timed_gpios);
 
 	ds2784_battery_init();
+	
+	serialno_setup(board_serialno());
 }
 
 static void __init bravo_fixup(struct machine_desc *desc, struct tag *tags,
